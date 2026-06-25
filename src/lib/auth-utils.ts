fef4d228a1e200062ed438f6e3431931eab5d5ea
Agustin Condori <agustincondori40@gmail.com>
2026-06-25 20:32:37 -0300

    se soluciono un error de tipeo

--- src/lib/auth-utils.ts
+++ src/lib/auth-utils.ts
@@ -2,7 +2,7 @@ import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export async function requireAnalyst() {
-  const { userId } = await auth();
+  const { userId, sessionId } = await auth();
   if (!userId) redirect("/sign-in");
 
   const client = await clerkClient();
